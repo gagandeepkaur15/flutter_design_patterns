200f9399edf88cfbd126eb78d62adb1413aecc67
Gagandeep <gagandeepkaur1523@gmail.com>
2023-08-15 18:48:02 +0530

    Abstract factory

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:design_patterns/abstract_factory.dart';
 import 'package:design_patterns/flutter_factory_method.dart';
 import 'package:flutter/material.dart';
 
@@ -14,33 +15,47 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //To convert this platform to widget type since it is inside build method we use build function on it
-          Platform.isAndroid ? PlatformButton(TargetPlatform.android).build(
-            () {
-              print("Android button pressed");
-            },
-            const Text('Click'),
-          ): 
-          PlatformButton(TargetPlatform.android).build(
-            () {
-              print("Android button pressed");
-            },
-            const Text('Click'),
-          ),
-          //Or use Theme.of(context).platform which returns platform which we are on
-          //Instead of using if and else conditions4 
-          PlatformButton(Theme.of(context).platform).build(
-            () {
-              print("Button Pressed");
-            },
-            const Text('Click'),
-          ),
-        ],
-      ),
-    ); 
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            // FACTORY METHOD
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //To convert this platform to widget type since it is inside build method we use build function on it
+                Platform.isAndroid
+                    ? PlatformButton(TargetPlatform.android).build(
+                        () {
+                          print("Android button pressed");
+                        },
+                        const Text('Click'),
+                      )
+                    : PlatformButton(TargetPlatform.android).build(
+                        () {
+                          print("Android button pressed");
+                        },
+                        const Text('Click'),
+                      ),
+                //Or use Theme.of(context).platform which returns platform which we are on
+                //Instead of using if and else conditions4
+                PlatformButton(Theme.of(context).platform).build(
+                  () {
+                    print("Button Pressed");
+                  },
+                  const Text('Click'),
+                ),
+              ],
+            ),
+
+            // ABSTRACT FACTORY
+            Column(
+              children: [
+                AbstractFactoryImpl().buildButton(context, 'Hello', () {}),
+                const SizedBox(height: 20),
+                AbstractFactoryImpl().buildIndicator(context),
+              ],
+            ),
+          ],
+        ));
   }
 }
---
+++ lib/platform_indicator_for_abstract_factory.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+abstract class PlatformIndicator {
+  Color color();
+  Widget build();
+
+  factory PlatformIndicator(TargetPlatform platform) {
+    switch (platform) {
+      case TargetPlatform.android:
+        return AndroidIndicator();
+      case TargetPlatform.iOS:
+        return IOSIndicator();
+      default:
+        return AndroidIndicator();
+    }
+  }
+}
+
+class AndroidIndicator implements PlatformIndicator {
+  @override
+  Widget build() {
+    return CircularProgressIndicator(
+      color: color(),
+    );
+  }
+
+  @override
+  Color color() {
+    return Colors.blue;
+  }
+}
+
+class IOSIndicator implements PlatformIndicator {
+  @override
+  Widget build() {
+    return CupertinoActivityIndicator(color: color());
+  }
+
+  @override
+  Color color() {
+    return Colors.red;
+  }
+}
